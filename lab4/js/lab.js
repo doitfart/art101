cb67b351b797a28b17d9645f2e83d67b9013b225
doitfart <130079983+doitfart@users.noreply.github.com>
2023-04-24 17:29:17 -0700

    la4 work + removed duplicate index
    see title

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,17 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Hangman game implemented in JS via pseudocoding. 
+// Author: Duncan Moran
+// Date: April 24 2023
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//Creating a HangMan game:
+//Generate a random 4 letter word and store it in the variable
+//Display the length of the 4 letter word to the user //Correct guess is less than the length of the word
+//Prompt the user to quess a letter
+  //If guessed correct
+    //increment the correct guess by 1
+  //If guessed incorrect
+    //increment the incorrect guess by 1
+    //Draw part of the hangman with each incorrect guess by 1
+//If guessed more than 2 time incorrectly, 
+  //inform the user
+//If all letters are guessed correctly
+//When they lose exit the program
