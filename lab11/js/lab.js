9adaa878c506d2443049024091d3fd4d7ad74a84
doitfart <130079983+doitfart@users.noreply.github.com>
2023-05-16 16:25:50 -0700

    lab 11
    added defer to lab template
    added lab 11 to homepage nav
    completed lab 11 (index, js, and css)

---
+++ lab11/js/lab.js
@@ -0,0 +1,25 @@
+// index.js - Lab 11: Libraries & jQuery - experiment with features of jQuery
+// Author: Duncan Moran
+// Date: May 16, 2023
+
+//add a button for each section (challenge, problems, and results)
+$("#challenge").append("<button id='challenge-button'>click?</button>");
+$("#problems").append("<button id='problems-button'>Click???</button>");
+$("#results").append("<button id='results-button'>CLICK?!?!</button>");
+
+//add a click event to each button for each section. challenge:
+$("#challenge-button").click(function(){
+    //wes' site also says a better/alternative anon function for here could be:
+    // $(this).parent().toggleClass("special"); which traverses the DOM to the parent. 
+    $("#challenge").toggleClass("special");
+})
+
+//for problems section:
+$("#problems-button").click(function(){
+    $("#problems").toggleClass("special");
+})
+
+//and finally for results section:
+$("#results-button").click(function(){
+    $("#results").toggleClass("special");
+})
