1a0781431fb906b295b82bd1b77d54da3c8dfde3
doitfart <130079983+doitfart@users.noreply.github.com>
2023-05-18 08:21:11 -0700

    comment explanations

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -9,7 +9,9 @@ $("#results").append("<button id='results-button'>CLICK?!?!</button>");
 
 //add a click event to each button for each section. challenge:
 $("#challenge-button").click(function(){
-    //wes' site also says a better/alternative anon function for here could be:
+    //wes' site also says a better/alternative anon function for here could be,
+    //which selectes "this" button. you can use this command when like 3 buttons have the sam class to select
+    //one specific button thats within the class.
     // $(this).parent().toggleClass("special"); which traverses the DOM to the parent. 
     $("#challenge").toggleClass("special");
 })
