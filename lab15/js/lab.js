2f1deb99ca298d32cd2c9bd1dafdb2305af5142b
doitfart <130079983+doitfart@users.noreply.github.com>
2023-05-30 21:12:58 -0700

    lab15
    just lab15 work in html, js, and css

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -1,3 +1,28 @@
 // index.js - Lab 15: AJAX - experiment with jQuery and AJAX
 // Author: Duncan Moran
 // Date: May 30, 2023
+
+//function to utilize API and grab info
+function getJoke() {
+    //debug
+    console.log("button clicked");
+    $.ajax({
+        //URL for request
+        url: "https://api.chucknorris.io/jokes/random",
+        //get request for data
+        type: "GET",
+        data: {},
+    })
+    //function for what to do with data from API
+    .done(function(data) {
+        //debug
+        console.log(data);
+        var joke = data.value;
+        var iconURL = data.icon_url;
+        $("#output").html("<h3>" + joke);
+        $("#output").append("<img src=" + iconURL + ">")
+    })  
+}
+
+//event listener to initiate getJoke function
+$("#activate").click(getJoke);
